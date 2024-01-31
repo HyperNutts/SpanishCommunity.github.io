60a71269ec4b80c1e63da26c111b3559cbf59d37
HyperNutts <129901225+HyperNutts@users.noreply.github.com>
2024-01-30 22:43:23 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+function irAAdministracion() {
+    // Agrega aquí la lógica para redirigir o mostrar la página de administración
+    alert("Ir a la página de Administración");
+}
+
+function irAVerFotos() {
+    // Agrega aquí la lógica para redirigir o mostrar la página de ver fotos
+    alert("Ir a la página de Ver Fotos");
+}
+
+function irAVerUsuarios() {
+    // Agrega aquí la lógica para redirigir o mostrar la página de ver usuarios
+    alert("Ir a la página de Ver Usuarios");
+}
